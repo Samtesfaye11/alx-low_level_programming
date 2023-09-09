1f25385f16075d3221a26f5f040a2905e13fa453
Samtesfaye11 <Samitesfayegk11@gmail.com>
2023-09-09 00:20:58 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-    char c;
- /* Initialize character variable c with 'a' */
-    c = 'a';
-    /* Print the lowercase alphabets using a while loop */
-    while (c <= 'z') {
-        putchar(c);
-        c++;
-    }
-
-    /* Print a newline character */
-    putchar('\n');
+char c;
+/* Initialize character variable c with 'a' */
+c = 'a';
+/* Print the lowercase alphabets using a while loop */
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+/* Print a newline character */
+putchar('\n');
 return (0);
 }
